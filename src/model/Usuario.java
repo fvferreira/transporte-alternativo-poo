8ec37b3e9568fe66b78bc3cbb36024f15687aff2
fvferreira <vicctor3472@gmail.com>
2026-05-31 10:34:55 -0300

    feat: Nova classe abstract Usuario

---
+++ src/model/Usuario.java
@@ -0,0 +1,77 @@
+
+public abstract class Usuario {
+
+    private String id;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private String senha;
+
+    public Usuario(String id, String nome, String cpf, String telefone, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public boolean realizarLogin(String email, String senha) {
+        return this.email.equals(email) && this.senha.equals(senha);
+    }
+
+    public void realizarCadastro(String id, String nome, String cpf, String telefone, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
